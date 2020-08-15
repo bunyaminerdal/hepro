13fb26927a4b8d6a1df9187e6a2d5cde073e4a5c
leonidas131 <leonidas131@hotmail.com>
2020-08-16 01:00:51 +0300

    Version 1.0.0

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const item = require("./routes/api/item");
+
+const app = express();
+
+//bodyparser Middleware
+app.use(bodyParser.json());
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//connect to mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("mongoDB connected..."))
+  .catch((err) => console.log(err));
+
+//USE Routes
+app.use("/api/items", item);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
