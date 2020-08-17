097df14ab22fbedbbf4253ef300a23e35eb4089c
leonidas131 <leonidas131@hotmail.com>
2020-08-17 10:02:44 +0300

    second-commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import AppNavbar from "./components/AppNavbar";
-import ShoppingList from "./components/ShoppingList";
-import ItemModal from "./components/itemModal";
+
 import { Container } from "reactstrap";
+import Home from "./home";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -10,9 +10,10 @@ import { loadUser } from "./actions/authActions";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import moduleName from "";
 
 class App extends Component {
-  componentDidMount() {
+  componentWillMount() {
     store.dispatch(loadUser());
   }
 
@@ -22,8 +23,7 @@ class App extends Component {
         <div className="App">
           <AppNavbar />
           <Container>
-            <ItemModal />
-            <ShoppingList />
+            <Home />
           </Container>
         </div>
       </Provider>
